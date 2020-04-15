fd67bf34a7f06adef83a30ee803019f2845a929c
GuillaumeLaine <guillaume.william1@hotmail.com>
2020-04-15 19:14:07 +0200

    working shading on full scene for td1

--- td1.cpp
+++ td1.cpp
@@ -22,10 +22,6 @@ public:
         return *this;
     }
 
-    void print_coords() {
-        cout << coords[0] << ' ' << coords[1] << ' ' << coords[2] << '\n';
-    } 
-
     const double& operator[](int i) const {return coords[i]; }
     double& operator[](int i) { return coords[i]; }
 
@@ -46,18 +42,17 @@ Vector scale(double a, const Vector& v) {
     return  Vector(a * v[0], a * v[1], a * v[2]);
 }
 
+double dot(const Vector& a, const Vector& b) {
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
 double norm(const Vector& a) {
-    return sqrt(pow(a[0], 2) + pow(a[1], 2) + pow(a[2], 2));
+    return sqrt(dot(a, a));
 }
 
 Vector unit(const Vector& a) {
     return scale(1/norm(a), a);
 }
-
-double dot(const Vector& a, const Vector& b) {
-    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
-}
-
 
 struct Intersection {
 
@@ -93,11 +88,12 @@ public:
         Intersection i;
         double d = dot(r.u, r.O - C);
         
-        double delta = pow(d, 2) - (pow(norm(r.O - C), 2) - (R * R));
+        double delta = d * d - (pow(norm(r.O - C), 2) - (R * R));
         if (delta >= 0) {
             double sq_delta = sqrt(delta);
-            double t1 = d - sq_delta;
-            double t2 = d + sq_delta;
+            double minus_d = dot(r.u, C - r.O);
+            double t1 = minus_d - sq_delta;
+            double t2 = minus_d + sq_delta;
             if (t2 >= 0) {
                 if (t1 >= 0) {
                     i.flag = true;
@@ -107,10 +103,10 @@ public:
                     i.flag = true;
                     i.P = r.O + scale(t2, r.u);
                 }
-                Vector n = i.P - C;
-                i.N = unit(n);
+
+                i.N = unit(i.P - C);
+                i.albedo = albedo;
             }
-            i.albedo = albedo;
         }
 
         return i;
@@ -138,8 +134,7 @@ public:
             Sphere sphere = s[i];
             Intersection intersection = sphere.intersect(r);
             if (intersection.flag) {
-                Vector cam_pos = Vector(0, 0, 55);
-                double dist = norm(cam_pos - intersection.P);
+                double dist = norm(r.O - intersection.P);
                 if (dist <= min_dist) {
                     min_dist = dist;
                     intersect_point = intersection;
@@ -169,7 +164,7 @@ double intensity(Scene& scene, Intersection& i, Vector& S) {
     int vp = 0;
 
     //Check visibility
-    Ray r(i.P + scale(0.01*norm(wi), wi), wi);
+    Ray r(i.P + scale(0.01, i.N), wi);
     Intersection light_i = scene.intersect(r);
 
     if (!light_i.flag) {
@@ -177,49 +172,50 @@ double intensity(Scene& scene, Intersection& i, Vector& S) {
     }
 
     else {
-        if (norm(light_i.P - S) >= d) {
+        if (norm(light_i.P - i.P) > d) {
             vp = 1;
         }
     }
 
-    return (I * vp * max(-dot(i.N, wi), 0.) / (4 * M_PI * M_PI * d * d));
+    return (I * vp * max(dot(i.N, wi), 0.) / (4 * M_PI * M_PI * d * d));
 };
 
 
 int main() {
 
     // Define scene
-    Sphere s(Vector(0, 0, 0), 10, Vector(1, 0, 1));
-    /*
+    Sphere s(Vector(0, 0, 0), 10, Vector(1, 1, 1));
     Sphere s1(Vector(0, 1000, 0), 940, Vector(1, 0, 0)); 
     Sphere s2(Vector(0, 0, -1000), 940, Vector(0, 1, 0));
     Sphere s3(Vector(0, -1000, 0), 990, Vector(0, 0, 1));
     Sphere s4(Vector(0, 0, 1000), 940, Vector(1, 0, 1));
-    */
+    Sphere s5(Vector(-1000, 0, 0), 940, Vector(0, 1, 1));
+    Sphere s6(Vector(1000, 0, 0), 940, Vector(1, 1, 0));
 
     vector<Sphere> scene_vector;
     scene_vector.push_back(s);
-    /*
     scene_vector.push_back(s1);
     scene_vector.push_back(s2);
     scene_vector.push_back(s3);
     scene_vector.push_back(s4);
-    */
+    scene_vector.push_back(s5);
+    scene_vector.push_back(s6);
 
     Scene scene(scene_vector);
     Vector S(-10, 20, 40);
 
     // Viewing window
-    int H = 400;
-    int W = 400;
+    int H = 512;
+    int W = 512;
     Vector cam(0, 0, 55);
     double fov = M_PI / 3;
     double z = cam[2] - W/(2*tan(fov/2));
+    double gamma = 1. / 2.2;
 
     // Ray trace
-    vector<unsigned char> img;
-    for (int i = 0; i < W; i++) {
-        for (int j = 0; j < H; j++) {
+    vector<unsigned char> img(W*H*3);
+    for (int i = 0; i < H; i++) {
+        for (int j = 0; j < W; j++) {
             double x = j;
             double y = H - i - 1;
             Vector pixel(
@@ -227,22 +223,22 @@ int main() {
                 cam[1] + y + 0.5 - H/2,
                 z
             );
-            Vector ray_dir = pixel - cam;
-            ray_dir = unit(ray_dir);
-            Ray r(pixel, ray_dir);
+            Vector ray_dir = unit(pixel - cam);
+            Ray r(cam, ray_dir);
 
             Intersection intersect = scene.intersect(r);
             if (intersect.flag) {
                 Vector albedo = intersect.albedo;
                 double L = intensity(scene, intersect, S);
-                img.push_back(albedo[0] * L * 255);
-                img.push_back(albedo[1] * L * 255);
-                img.push_back(albedo[2] * L * 255);
+                double throuput = L * 255;
+                img[(i * W + j)*3 + 0] = max(0., min(255., pow(albedo[0] * throuput, gamma)));
+                img[(i * W + j)*3 + 1] = max(0., min(255., pow(albedo[1] * throuput, gamma)));
+                img[(i * W + j)*3 + 2] = max(0., min(255., pow(albedo[2] * throuput, gamma)));
             }
             else {
-                img.push_back(0);
-                img.push_back(0);
-                img.push_back(0);
+                img[(i * W + j)*3 + 0] = 0;
+                img[(i * W + j)*3 + 1] = 0;
+                img[(i * W + j)*3 + 2] = 0;
             }
 
         }
